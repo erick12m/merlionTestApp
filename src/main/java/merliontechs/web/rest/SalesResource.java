f2f2fd66e0c204aa632fc58eeb1049a4f02368bb
Erick Martinez <erickm1299@gmail.com>
2020-08-17 16:56:37 -0300

    Add: Funcion para obtener la cantidad de ventas por dia

--- src/main/java/merliontechs/web/rest/SalesResource.java
+++ src/main/java/merliontechs/web/rest/SalesResource.java
@@ -11,10 +11,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-import sun.jvm.hotspot.ui.SAEditorPane;
+
 
 import java.net.URI;
 import java.net.URISyntaxException;
